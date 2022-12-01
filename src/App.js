1f5e614e74eecd1512ef0bfe765e2696cf56530f
Jennifer Haggerty <thejenniferhaggerty@protonmail.com>
2022-11-30 22:23:03 -0500

    Initial layout set. Sass installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+	<div className="App">
+		<header className={styles.banner}></header>
+		<nav className={styles.navigation}></nav>
+		<main className={styles.main}>
+			<section className={styles.leftPanel}>
+				<div className={styles.profilePic}></div>
+				<div className={styles.contact}></div>
+				<div className={styles.interests}></div>
+				<div className={styles.details}></div>
+				<div className={styles.school}></div>
+				<div className={styles.networking}></div>
+				<div className={styles.companies}></div>
+			</section>
+			<section className={styles.rightPanel}>
+				<div className={styles.network}></div>
+				<div className={styles.blog}></div>
+				<div className={styles.blurbs}></div>
+				<div className={styles.friends}></div>
+				<div className={styles.comments}></div>
+			</section>
+		</main>
+		<footer className={styles.footer}></footer>
+	</div>
+	);
 }
 
 export default App;
