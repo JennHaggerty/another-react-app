0044d6f97c49ad4084f39d65453a4e6f154e7bf9
Jennifer Haggerty <thejenniferhaggerty@protonmail.com>
2022-12-01 00:17:14 -0500

    setup profile pic, set contacts

--- src/App.js
+++ src/App.js
@@ -9,8 +9,17 @@ class App extends Component {
   
 	  this.state = {
 		user: {
-			name: "Jennifer",
+			name: {
+				first: "Jennifer",
+				last: "Haggerty"
+			},
+			email: "thejenniferhaggerty@protonmail.com",
+			location: "Remote, GEORGIA United States",
+			website: "https://studiovonjenn.com",
+			age: 100,
+			pronouns: "She/xe",
 			photo: profilePic,
+			quote: "wee :D",
 		}
 	  };
 	}
@@ -18,32 +27,102 @@ class App extends Component {
 	render (){
 		const user = this.state.user;
 
-		const ProfilePicSection = () => {
+		const PhotoBox = () => {
 			return (
 				<div className={styles.profilePic}>
-					<h1 className={styles.h1}>{user.name}</h1>
+					<span className={styles.h1}>{user.name.first}</span>
 					<div className="">
 						<div className={styles.leftPanel}>
 							<img 
+							height="138px"
+							width="170px"
 								src={user.photo}
-								alt={user.name + "'s profile photo"}
+								alt={user.name.first + "'s profile photo"}
 							/>
 						</div>
 						<div className={styles.rightPanel}>
-							
+							<div className={styles.row}>
+								"{user.quote}"
+							</div>
+							<div className={styles.row}>
+								{user.pronouns}
+								<br />
+								{user.age} years old
+							</div>
+							<div className={styles.row}>
+								{user.location}
+							</div>
+							<div className={styles.row}>
+								Last updated: today
+							</div>
 						</div>
+						<a href={user.website}>View more pics</a>
 					</div>
 				</div>
 			)
 		}
+
+		const ContactBox = () => {
+			return(
+				<div className={styles.contact}>
+					<h2>Contacting {user.name.first}</h2>
+					<table>
+						<tbody>
+							<tr>
+								<th>
+									Email
+								</th>
+								<td>
+									<a href={"mailto:" + user.email}>
+										Send Message
+									</a>
+								</td>
+								{/*
+								<th>
+									Current Website
+								</th>
+								<td>
+									<button
+										type="button"
+										className={styles.btnAsLink}
+										onClick={() => {
+											window.navigator.clipboard.writeText(window.location.href);
+										}}
+										aria-label="Click to copy url and share it."
+									>
+										Forward to friend
+									</button>
+								</td>
+								*/}
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			)
+		}
+
 		return (
-			<div className="App">
-				<header className={styles.banner}></header>
-				<nav className={styles.navigation}></nav>
+			<div className={styles.app}>
+				{/* 
+				<header className={styles.banner} aria-hidden="true">
+					<span className={styles.hidden}>
+						<h2 className={styles.hidden}>Search:</h2>
+						For now this is a placeholder to display the pretty blue banner.
+						TODO: Add the Search bar feature that will search and highlight terms in page or,
+							if the user selects search the web we'll send them over to Brave.
+					</span>
+				</header>
+				<nav className={styles.navigation} aria-hidden="true">
+					<span className={styles.hidden}>
+						<h2 className={styles.hidden}>Navigation:</h2>
+						Navigation placeholder.
+					</span>
+				</nav>
+				*/}
 				<main className={styles.main}>
 					<section className={styles.leftPanel}>
-						<ProfilePicSection />
-						<div className={styles.contact}></div>
+						<PhotoBox />
+						<ContactBox />
 						<div className={styles.interests}></div>
 						<div className={styles.details}></div>
 						<div className={styles.school}></div>
@@ -51,47 +130,21 @@ class App extends Component {
 						<div className={styles.companies}></div>
 					</section>
 					<section className={styles.rightPanel}>
-						<div className={styles.network}></div>
+						<div className={styles.network}>
+							<h1>{user.name.first} is a web developer in your virtual network</h1>
+						</div>
 						<div className={styles.blog}></div>
 						<div className={styles.blurbs}></div>
 						<div className={styles.friends}></div>
 						<div className={styles.comments}></div>
 					</section>
 				</main>
+				{/*
 				<footer className={styles.footer}></footer>
+				*/}
 			</div>
 		)
 	}
 }
-
-/*
-function App() {
-	return (
-	<div className="App">
-		<header className={styles.banner}></header>
-		<nav className={styles.navigation}></nav>
-		<main className={styles.main}>
-			<section className={styles.leftPanel}>
-				<ProfilePicSection />
-				<div className={styles.contact}></div>
-				<div className={styles.interests}></div>
-				<div className={styles.details}></div>
-				<div className={styles.school}></div>
-				<div className={styles.networking}></div>
-				<div className={styles.companies}></div>
-			</section>
-			<section className={styles.rightPanel}>
-				<div className={styles.network}></div>
-				<div className={styles.blog}></div>
-				<div className={styles.blurbs}></div>
-				<div className={styles.friends}></div>
-				<div className={styles.comments}></div>
-			</section>
-		</main>
-		<footer className={styles.footer}></footer>
-	</div>
-	);
-}
-*/
 
 export default App;
