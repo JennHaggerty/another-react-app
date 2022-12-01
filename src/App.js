6e627b45df5d496e555d8361560d11c8f653e9a3
Jennifer Haggerty <thejenniferhaggerty@protonmail.com>
2022-11-30 23:10:58 -0500

    Converted to compoent added initial images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,70 @@
+import React, { Component } from 'react';
 import styles from './App.module.scss';
 
+import profilePic from "./assets/jpg/jennifer_portrait.JPG";
+
+class App extends Component {
+	constructor(props) {
+	  super(props);
+  
+	  this.state = {
+		user: {
+			name: "Jennifer",
+			photo: profilePic,
+		}
+	  };
+	}
+
+	render (){
+		const user = this.state.user;
+
+		const ProfilePicSection = () => {
+			return (
+				<div className={styles.profilePic}>
+					<h1 className={styles.h1}>{user.name}</h1>
+					<div className="">
+						<div className={styles.leftPanel}>
+							<img 
+								src={user.photo}
+								alt={user.name + "'s profile photo"}
+							/>
+						</div>
+						<div className={styles.rightPanel}>
+							
+						</div>
+					</div>
+				</div>
+			)
+		}
+		return (
+			<div className="App">
+				<header className={styles.banner}></header>
+				<nav className={styles.navigation}></nav>
+				<main className={styles.main}>
+					<section className={styles.leftPanel}>
+						<ProfilePicSection />
+						<div className={styles.contact}></div>
+						<div className={styles.interests}></div>
+						<div className={styles.details}></div>
+						<div className={styles.school}></div>
+						<div className={styles.networking}></div>
+						<div className={styles.companies}></div>
+					</section>
+					<section className={styles.rightPanel}>
+						<div className={styles.network}></div>
+						<div className={styles.blog}></div>
+						<div className={styles.blurbs}></div>
+						<div className={styles.friends}></div>
+						<div className={styles.comments}></div>
+					</section>
+				</main>
+				<footer className={styles.footer}></footer>
+			</div>
+		)
+	}
+}
+
+/*
 function App() {
 	return (
 	<div className="App">
@@ -7,7 +72,7 @@ function App() {
 		<nav className={styles.navigation}></nav>
 		<main className={styles.main}>
 			<section className={styles.leftPanel}>
-				<div className={styles.profilePic}></div>
+				<ProfilePicSection />
 				<div className={styles.contact}></div>
 				<div className={styles.interests}></div>
 				<div className={styles.details}></div>
@@ -27,5 +92,6 @@ function App() {
 	</div>
 	);
 }
+*/
 
 export default App;
