8e049415303dbffbfaf14461d25cc97381a23b8e
Jennifer Haggerty <thejenniferhaggerty@protonmail.com>
2023-06-26 19:51:55 -0400

    Updates

--- src/components/theme-selector/variables_2004.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import "../../assets/stylesheets/variables_2004.scss";
-
-export const Variables2004 = (React.FC = () => <></>);
-export default Variables2004;
--- src/components/theme-selector/variables_2004_accessible.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import "../../assets/stylesheets/variables_2004_accessible.scss";
-
-export const Variables2004Acessible = (React.FC = () => <></>);
-export default Variables2004Acessible;
